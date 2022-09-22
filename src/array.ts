ae396ab6ef99196616ed9c4c57b259b246938d03
Aybuai <13624448268@163.com>
2022-09-22 23:50:59 +0800

    feat: basic & array

---
+++ src/array.ts
@@ -0,0 +1,6 @@
+// array & tuple 元组
+let arrOfNumber: number[] = [1, 2];
+arrOfNumber.push(12);
+
+let user: [string, number] = ["aybuai", 19];
+user.push("haha");
---
+++ src/basic.ts
@@ -0,0 +1,16 @@
+// 7种原始类型
+// number string boolean bigint undefined null symbol
+
+let isDone: boolean = false;
+
+isDone = true;
+
+let myName: string = `Aybuai`;
+
+let age: number = 18;
+
+let message: string = `hello, ${myName}`;
+
+let u: undefined = undefined;
+
+let n: null = null;

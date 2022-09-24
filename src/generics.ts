b7ecca7f012fa1752436f4b3b8ca191b5f100725
Aybuai <13624448268@163.com>
2022-09-24 16:54:38 +0800

    feat: generics

--- src/generics.ts
+++ src/generics.ts
@@ -49,3 +49,40 @@ function echoWithLength<T extends IsWithLength>(arg: T): T {
 const str = echoWithLength("str");
 const obj = echoWithLength({ length: 1 });
 const arr = echoWithLength([1, 2]);
+
+// 泛型还可以应用到 interface、class等等，起到约束 interface、class作用
+
+// 声明一个通用队列
+class Queue<T> {
+  private data: T[] = [];
+
+  push(value: T) {
+    return this.data.push(value);
+  }
+
+  size() {
+    return this.data.length;
+  }
+
+  // 移除后，队列为空时，数据类型是 undefined
+  pop(): T | undefined {
+    return this.data.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.push(1);
+console.log("队列的长度", queue.size());
+queue.pop()?.toFixed();
+console.log("pop后，队列的长度", queue.size());
+
+interface keyPair<T, U> {
+  key: T;
+  value: U;
+}
+
+const kp1: keyPair<number, string> = { key: 1, value: "name" };
+
+// 下面声明数组是一样的
+const arr2: Array<number> = [1, 2];
+const arr3: number[] = [1, 2];

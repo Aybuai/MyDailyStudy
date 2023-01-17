c363deac3ef50b9fb16c9bd87b407e4f19a167c2
Aybuai <13624448268@163.com>
2023-01-17 08:46:42 +0800

    fix：format code

--- src/JS/this_direction.js
+++ src/JS/this_direction.js
@@ -75,12 +75,12 @@ function Foo() {
   return o;
 }
 
-const instance = new Foo();
+const instance = new Foo(); 
 console.log(instance.user, "11");
 
 function Foo2() {
   this.user = "Lucas";
-  // 显式返回一个基本类型，则仍然指向实例
+  // 显式返回一个基本类型，则仍然指向实例。
   return 1;
 }
 
